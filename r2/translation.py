f3915eb3b8180254fd5de5249f15e35086cb7056
Colin Woodbury <colingw@gmail.com>
2011-06-21 21:53:22 +0900

    Updated translation.py
    - Made translation.py 's python a little prettier.

--- r2/translation.py
+++ r2/translation.py
@@ -19,20 +19,23 @@
 # All portions of the code written by CondeNet are Copyright (c) 2006-2010
 # CondeNet, Inc. All Rights Reserved.
 ################################################################################
-from distutils.cmd import Command
-from babel.messages.frontend import new_catalog as _new_catalog, \
-     extract_messages as _extract_messages
-from distutils.cmd import Command
-from distutils.errors import DistutilsOptionError
-from babel import Locale
-import os, shutil, re
 
+# Making your Python prettier.  -- fosskers
+
+from babel.messages.frontend import new_catalog      as _new_catalog,
+                                    extract_messages as _extract_messages
+from distutils.errors        import DistutilsOptionError
+from distutils.cmd           import Command
+from babel                   import Locale
+import shutil
+import os
+import re
 
 
 class extract_messages(_extract_messages):
     def initialize_options(self):
         _extract_messages.initialize_options(self)
-        self.output_file = 'r2/i18n/r2.pot'
+        self.output_file  = 'r2/i18n/r2.pot'
         self.mapping_file = 'babel.cfg'
 
 class new_catalog(_new_catalog):
@@ -40,25 +43,26 @@ class new_catalog(_new_catalog):
         _new_catalog.initialize_options(self)
         self.output_dir = 'r2/i18n'
         self.input_file = 'r2/i18n/r2.pot'
-        self.domain = 'r2'
+        self.domain     = 'r2'
 
     def finalize_options(self):
         _new_catalog.finalize_options(self)
         if os.path.exists(self.output_file):
             file2 = self.output_file + "-sav"
-            print " --> backing up existing PO file to '%s'" % file2
+            print " --> backing up existing PO file to '{0}'".format(file2)
             shutil.copyfile(self.output_file, file2)
 
 class commit_translation(Command):
-    description = 'Turns PO into MO files'
+    '''Converts PO files to MO files.'''
+    description  = 'Turns PO into MO files'
     user_options = [('locale=', 'l',
                      'locale for the new localized catalog'), ]
 
     def initialize_options(self):
-        self.locale = None
+        self.locale     = None
         self.output_dir = 'r2/i18n'
-        self.domain = 'r2'
-        self.string = '_what_'
+        self.domain     = 'r2'
+        self.string     = '_what_'
 
     def finalize_options(self):
         if not self.locale:
@@ -70,12 +74,12 @@ class commit_translation(Command):
         self.output_file = os.path.join(self.output_dir, self.locale,
                                         'LC_MESSAGES', self.domain + '.mo')
     def run(self):
-        cmd = 'msgfmt -o "%s" "%s"' % (self.output_file, self.input_file)
-        handle = os.popen(cmd)
-        print handle.read(),
-        handle.close()
+        cmd = 'msgfmt -o "{0}" "{1}"'.format(self.output_file, self.input_file)
+        with os.popen(cmd) as handle:
+            print handle.read(),
 
 class test_translation(new_catalog):
+    '''Makes a mock-up PO and MO file for testing and sets to en.'''
     description = 'makes a mock-up PO and MO file for testing and sets to en'
     user_options = [('locale=', 'l',
                      'locale for the new localized catalog'),
@@ -102,37 +106,34 @@ class test_translation(new_catalog):
 
     def run(self):
         new_catalog.run(self)
-        handle = open(self.output_file)
-        res = ''
-        counter = 0
-        formatting_string = False
-        for line in handle:
-            if not ('""' in line):
-                strlen = len(re.findall(r"\S+", line)) - 1
-            if "%" in line:
-                formatting_string = re.findall(r"%\S+", line)
-                strlen -= len(formatting_string)
-                formatting_string = (', '.join(formatting_string)).strip('"')
-            if '""' in line and not("msgid" in line):
-                strlen = 1 if strlen < 1 else strlen
-                string = ' '.join([self.string for x in range(0, strlen)])
-                if formatting_string:
-                    string = '%s [%s]' %(string, formatting_string)
-                    formatting_string = ''
-                string = '"%s"' % string
-                res += line if counter < 1 else line.replace('""', string)
-                counter += 1
-            elif line.startswith('"Last-Translator:'):
-                res += line.replace("FULL NAME", "Babel Phish")
-            else:
-                res += line
-        handle.close()
-        handle = open(self.output_file, 'w')
-        handle.write(res)
-        handle.close()
-        cmd = 'msgfmt -o "%s" "%s"' % (self.output_file_mo, self.output_file)
-        handle = os.popen(cmd)
-        print "converting to MO file..."
-        print handle.read(),
-        handle.close()
-
+        with open(self.output_file) as handle:
+            res = ''
+            counter = 0
+            formatting_string = None
+            for line in handle:
+                if '""' not in line:
+                    strlen = len(re.findall(r"\S+", line)) - 1
+                if "%" in line:
+                    formatting_string = re.findall(r"%\S+", line)
+                    strlen -= len(formatting_string)
+                    formatting_string = ', '.join(formatting_string).strip('"')
+                if '""' in line and 'msgid' not in line:
+                    strlen = 1 if strlen < 1 else strlen
+                    string = ' '.join([self.string for x in xrange(strlen)])
+                    if formatting_string:
+                        string = '{0} [{1}]'.format(string, formatting_string)
+                        formatting_string = ''
+                    string = '"{0}"'.format(string)
+                    res += line if counter < 1 else line.replace('""', string)
+                    counter += 1
+                elif line.startswith('"Last-Translator:'):
+                    res += line.replace("FULL NAME", "Babel Phish")
+                else:
+                    res += line
+        with open(self.output_file, 'w') as handle:
+            handle.write(res)
+        cmd = 'msgfmt -o "{0}" "{1}"'.format(self.output_file_mo, 
+                                             self.output_file)
+        with os.popen(cmd) as handle:
+            print "converting to MO file..."
+            print handle.read(),
